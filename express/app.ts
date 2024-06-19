19eeac1c911f22523473d8e15f7a3992d789ed1b
Gerald Simpson <gtnsimpson@gmail.com>
2024-06-19 18:34:12 +0100

    Displayed data in next app from express api.

--- express/app.ts
+++ express/app.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World! TEST!');
+  res.json({ data: 'Hello World! TEST!' });
 });
 
 app.listen(port, () => {
---
+++ next/to_do_list/src/pages/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Space_Mono } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const space = Space_Mono({ weight: ['400', '700'], subsets: ['latin'] });
