c932e29357c7dde7f1bd4e93d9ba80c5c2b9951f
Gerald-Simpson <gtnsimpson@gmail.com>
2024-06-23 18:13:34 +0100

    Updated delete title route to not throw a zod type error if there are no items to delete prior to deleting the title.

--- next/to_do_list/src/pages/index.tsx
+++ next/to_do_list/src/pages/index.tsx
@@ -1,60 +1,79 @@
 import Image from 'next/image';
 import { space, inter } from './fonts';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { useRouter } from 'next/navigation';
 
 // To be used once api setup
 type listTitles = {
-  id: string;
-  cookieId: string;
-  createdAt: Date;
-  title: string;
+    id: string;
+    cookieId: string;
+    createdAt: Date;
+    title: string;
 };
 
 export const getServerSideProps = (async (context) => {
-  let cookieId = context.req.cookies.id;
-  console.log(cookieId);
-  // on first load, cookieId will be undefined, so extracted from headers.
-  let firstLoadCookieId = context.res.getHeader('set-cookie');
-  if (!cookieId && typeof firstLoadCookieId === 'object') {
-    cookieId = firstLoadCookieId[0].slice(3, -8);
-  }
+    let cookieId = context.req.cookies.id;
+    console.log(cookieId);
+    // on first load, cookieId will be undefined, so extracted from headers.
+    let firstLoadCookieId = context.res.getHeader('set-cookie');
+    if (!cookieId && typeof firstLoadCookieId === 'object') {
+        cookieId = firstLoadCookieId[0].slice(3, -8);
+    }
 
-  let getUrl: string =
-    process.env.EXPRESS_HOST_NAME! + '/fetchLists/?cookieId=' + cookieId;
+    let getUrl: string =
+        process.env.EXPRESS_HOST_NAME! + '/fetchLists/?cookieId=' + cookieId;
 
-  const res = await fetch(getUrl, {
-    method: 'GET',
-  });
-  const listTitles: { listTitles: listTitles[] } = await res.json();
+    const res = await fetch(getUrl, {
+        method: 'GET',
+    });
+    const listTitles: { listTitles: listTitles[] } = await res.json();
 
-  return { props: { listTitles: listTitles.listTitles, cookieId: cookieId } };
+    return { props: { listTitles: listTitles.listTitles, cookieId: cookieId } };
 }) satisfies GetServerSideProps<{ listTitles: listTitles[]; cookieId: any }>;
 
 export default function Page({
-  listTitles,
-  cookieId,
+    listTitles,
+    cookieId,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return (
-    <main
-      className={
-        'flex min-h-screen flex-col items-center justify-between p-24 ' +
-        space.className
-      }
-    >
-      <div>Welcome</div>
-      {listTitles.map((title) => {
-        return (
-          <div className='flex flex-row'>
-            <div className='mr-4'>{title.title}</div>
-            <div className='mr-4'>
-              {new Date(title.createdAt).toDateString().slice(4)}
+    let router = useRouter();
+    const deleteTitle = async (id: number) => {
+        //do nothing
+        console.log(id);
+        router.refresh();
+        console.log('refreshed');
+    };
+    console.log(listTitles);
+    return (
+        <main
+            className={'flex min-h-screen w-full flex-col m-2 ' + space.className}
+        >
+            <h1 className='flex w-full text-xl my-2'>My Notes:</h1>
+            {listTitles.map((title) => {
+                return (
+                    <div className='flex flex-row my-1 text-sm' key={title.id}>
+                        <div
+                            className='mr-4'
+                            onClick={() => deleteTitle(parseInt(title.id))}
+                        >
+                            X
+                        </div>
+                        <div className='mr-4'>{title.title}</div>
+                        <div className='mr-4'>^</div>
+                    </div>
+                );
+            })}
+            <div className='flex flex-row my-1 text-sm'>
+                <div className='mr-4 ml-6'>Input new list...</div>
+                <div className='mr-4'>*</div>
             </div>
-            <div className='mr-4'>expand</div>
-            <div className='mr-4'>delete</div>
-          </div>
-        );
-      })}
-      <div>footer</div>
-    </main>
-  );
+        </main>
+    );
 }
+
+/*
+ 
+                        <div className='mr-4'>
+                            {new Date(title.createdAt).toDateString().slice(4)}
+                        </div>
+
+*/
